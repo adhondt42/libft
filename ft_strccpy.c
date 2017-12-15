1508537cab56939aaf4a2b82ae4ca187cde9560f
Axel DHONDT <adhondt@e3r5p3.42.fr>
2017-12-15 17:37:35 +0100

    M.A.J. LIBFT

--- ft_strccpy.c
+++ ft_strccpy.c
@@ -6,12 +6,12 @@
 /*   By: adhondt <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/10 11:31:53 by adhondt           #+#    #+#             */
-/*   Updated: 2017/12/15 16:40:00 by adhondt          ###   ########.fr       */
+/*   Updated: 2017/12/15 16:47:13 by adhondt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
+// ne copie pas le caractere recherche
 char	*ft_strccpy(char *dest, const char *src, char c)
 {
 	int	i;
@@ -21,8 +21,7 @@ char	*ft_strccpy(char *dest, const char *src, char c)
 	{
 		if (src [i] == c)
 		{
-			dest[i] = c;
-			dest[i + 1] = '\0';
+			dest[i] = '\0';
 			return (dest);
 		}
 		dest[i] = src[i];
