fa917c7ba488c03e563974db78c2abfdafb42267
adhondt42 <adhondt@student.42.fr>
2018-03-21 16:25:03 +0100

    one more commit

--- ft_strdup.c
+++ ft_strdup.c
@@ -3,27 +3,30 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: adhondt <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: adhondt  <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/11/08 09:26:35 by adhondt           #+#    #+#             */
-/*   Updated: 2017/11/23 11:42:09 by adhondt          ###   ########.fr       */
+/*   Created: 2017/11/16 11:08:26 by adhondt           #+#    #+#             */
+/*   Updated: 2017/11/23 15:21:40 by adhondt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strdup(const char *tab)
+char	*ft_strdup(const char *s)
 {
-	char	*dest;
-	char	*src;
 	int		i;
+	char	*dest;
 
-	src = (char *)tab;
-	i = ft_strlen(tab);
-	if ((dest = (char*)malloc(sizeof(*src) * i + 1)) == NULL)
-		return (NULL);
-	dest[i] = '\0';
-	while (i-- >= 0)
-		dest[i] = src[i];
+	i = 0;
+	dest = (char *)malloc(sizeof(char) * (ft_strlen(s) + 1));
+	if (dest != NULL)
+	{
+		while (s[i] != '\0')
+		{
+			dest[i] = s[i];
+			i++;
+		}
+		dest[i] = s[i];
+	}
 	return (dest);
 }
